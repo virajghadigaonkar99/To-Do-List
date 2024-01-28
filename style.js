ffd8856d837821ac02b014544aa364b8d40df0c5
Viraj Ghadigaonkar <120920814+virajghadigaonkar99@users.noreply.github.com>
2024-01-28 13:55:05 +0530

    Add files via upload

---
+++ style.js
@@ -0,0 +1,25 @@
+const item= document.querySelector("#item");
+const toDoBox = document.querySelector("#to-do-box");
+
+item.addEventListener("keyup",function(event){
+     if(event.key=="Enter"){
+        addTODo(this.value);
+        this.value=" ";
+     }
+});
+
+
+const addTODo=(item)=>{
+    const listitem= document.createElement("li");
+    listitem.innerHTML=` ${item} <i class="fa-solid fa-xmark"></i>`;
+
+    listitem.addEventListener("click",function(){
+        this.classList.toggle("done");
+    });
+
+    listitem.querySelector("i").addEventListener("click",function(){
+        listitem.remove();
+    });
+
+    toDoBox.appendChild(listitem);
+}
